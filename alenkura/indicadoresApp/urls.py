c6709910dcab10abf586bcb0ededb11e0e8fa126
CToph02 <christopher.munoz.0220@gmail.com>
2025-11-11 13:00:10 -0300

    CToph02: Urls de indicadores

---
+++ alenkura/indicadoresApp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import indicadores_create, indicadores_delete, indicadores_edit, indicadores_list
+urlpatterns = [
+    path('', indicadores_list.as_view(), name='indicadores_list'),
+    path('create/', indicadores_create.as_view(), name='indicadores_create'),
+    path('edit/<int:pk>/', indicadores_edit.as_view(), name='indicadores_edit'),
+    path('delete/<int:pk>/', indicadores_delete.as_view(), name='indicadores_delete'),
+]

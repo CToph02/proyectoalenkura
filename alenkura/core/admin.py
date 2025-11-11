7e68cfd9f71d4a4a22f36e783a2a5fdc29a23412
Bdiel <abdielbarrapino@icloud.com>
2025-11-11 00:16:49 -0300

    Core creado

---
+++ alenkura/core/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Asignatura, Eje, Contenido
+
+admin.site.register(Asignatura)
+admin.site.register(Eje)
+admin.site.register(Contenido)
---
+++ alenkura/core/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Asignatura(models.Model):
+    nombre = models.CharField(max_length=100)
+    objetivo_evaluacion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class Eje(models.Model):
+    nombre = models.CharField(max_length=100)
+    asignatura = models.ForeignKey(
+        Asignatura,
+        on_delete=models.CASCADE,
+        related_name='ejes'
+    )
+
+    def __str__(self):
+        return f"{self.nombre} ({self.asignatura.nombre})"
+
+
+class Contenido(models.Model):
+    nombre = models.CharField(max_length=150)
+    descripcion = models.TextField(blank=True)
+    eje = models.ForeignKey(
+        Eje,
+        on_delete=models.CASCADE,
+        related_name='contenidos'
+    )
+
+    def __str__(self):
+        return f"{self.nombre} ({self.eje.nombre})"

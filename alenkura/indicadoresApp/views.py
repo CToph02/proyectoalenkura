4c6790c56c6f56f5d3c7b0094a5caabbeac82d4b
CToph02 <christopher.munoz.0220@gmail.com>
2025-11-11 13:02:17 -0300

    CToph02: views indicador

--- alenkura/indicadoresApp/views.py
+++ alenkura/indicadoresApp/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Indicadores
 
 # Create your views here.
+class indicadores_list(LoginRequiredMixin, ListView):
+    pass
+    #model = Indicadores
+    #context_object_name = 'indicadores_list'
+
+class indicadores_create(LoginRequiredMixin, CreateView):
+    pass
+    # model = Indicadores
+    # fields = [
+    # 'profesor', 
+    # 'estudiante', 
+    # 'asignatura', 
+    # 'indicador', 
+    # 'puntaje', 
+    # 'puntaje_obtenido', 
+    # 'paci'
+    # ]
+    # success_url = reverse_lazy("indicadores:list")
+
+class indicadores_edit(LoginRequiredMixin, UpdateView):
+    pass
+    #model = Indicadores
+    # success_url = reverse_lazy("indicadores:list")
+
+class indicadores_delete(LoginRequiredMixin, DeleteView):
+    pass
+    #model = Indicadores
+    # success_url = reverse_lazy("indicadores:list")

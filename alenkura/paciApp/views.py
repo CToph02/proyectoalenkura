b118949d98ea454cec1ad0428456c3a40c5f1f7e
CToph02 <christopher.munoz.0220@gmail.com>
2025-11-11 13:02:42 -0300

    CToph02: views paci

--- alenkura/paciApp/views.py
+++ alenkura/paciApp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 # Create your views here.
+class paci_list(LoginRequiredMixin, ListView):
+    pass
+
+class paci_create(LoginRequiredMixin, CreateView):
+
+    fields = [
+    'nombre', 
+    'descripcion', 
+    'fecha_inicio', 
+    'fecha_fin', 
+    'estado']
+
+    success_url = reverse_lazy("paci:list")
+class paci_edit(LoginRequiredMixin, UpdateView):
+    success_url = reverse_lazy("paci:list")
+
+class paci_delete(LoginRequiredMixin, DeleteView):
+    success_url = reverse_lazy("paci:list")

8459d02966cbc2478368bddfcbf00199c28689ee
CToph02 <christopher.munoz.0220@gmail.com>
2025-11-11 13:02:29 -0300

    CToph02: modelo paci

--- alenkura/paciApp/models.py
+++ alenkura/paciApp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+from core.models import DateTime, Student, Subject, Axis
 
 # Create your models here.
+def validate_list_of_strings(value):
+    if not isinstance(value, list):
+        raise ValidationError("Estrategias debe ser una lista.")
+    for i, v in enumerate(value):
+        if not isinstance(v, str) or not v.strip():
+            raise ValidationError(f"Estrategia #{i+1} debe ser un string no vacío.")
+
+class PaciAppModel(DateTime):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='paci_student')
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='paci_subject')
+    axis = models.ForeignKey(Axis, on_delete=models.CASCADE, related_name='paci_axis')
+    objetivo_general = models.TextField()
+    adecuacion_curricular = models.TextField()
+
+    estrategias = models.JSONField(default=list, blank=True, validators=[validate_list_of_strings])
+
+    puntaje_total = models.SmallIntegerField()
+
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        verbose_name = "Paci App Model"
+        verbose_name_plural = "Paci App Models"

c0481b75a141a283fa62a31dbd771d5eb5f4a8a0
CToph02 <christopher.munoz.0220@gmail.com>
2025-12-01 19:25:13 -0300

    CToph02: no sé

--- alenkura/core/admin.py
+++ alenkura/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Asignatura, Eje, Contenido, Curso, Apoderado, Estudiante, Sala, Estrategias
+from .models import Asignatura, Eje, Contenido, Curso, Apoderado, Estudiante, Sala, Estrategias, Objetivos
 
 admin.site.register(Asignatura)
 admin.site.register(Eje)
@@ -9,3 +9,4 @@ admin.site.register(Apoderado)
 admin.site.register(Estudiante)
 admin.site.register(Sala)
 admin.site.register(Estrategias)
+admin.site.register(Objetivos)
